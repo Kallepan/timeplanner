b4d8b9363bdf487791623a4202e329d5f42c058d
kajeepan kandeepan <kajeepan.kandeepan@gmail.com>
2023-12-22 05:49:16 +0000

    feat: initialized auth-backend setup

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 # Install global dependencies
-NODE_MODULES="@angular/cli eslint"
+NODE_MODULES="@angular/cli"
 npm install -g ${NODE_MODULES} && \
     npm cache clean --force
 
-# install backend dependencies
-cd backend
+# install auth backend dependencies
+cd auth-backend
+go mod download
+go mod tidy
+
+# install planner backend dependencies
+cd ../planner-backend
 go mod download
 go mod tidy
 
