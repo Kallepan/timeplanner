1824ee29c80bba0aba6f5e5aa1f5672fe36c6aa8
kajeepan kandeepan <kajeepan.kandeepan@gmail.com>
2024-01-17 09:36:51 +0100

    fix: outdated urls

--- frontend/src/app/environments/environment.ts
+++ frontend/src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://pc-kandeepan.labmed.de:9000/api/v1',
-  authUrl: 'http://pc-kandeepan.labmed.de:9000/api/token',
-  verifyUrl: 'http://pc-kandeepan.labmed.de:9000/api/verify',
+  apiUrl: 'http://localhost:8080/api/v1',
+  authUrl: 'http://localhost:8080/auth',
+  verifyUrl: 'http://localhost:8080/api/me',
 };
