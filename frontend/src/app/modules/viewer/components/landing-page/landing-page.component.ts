0a90d6c2ffec97a94201bb6a9a64be9cb20d561f
kajeepan kandeepan <kajeepan.kandeepan@gmail.com>
2024-01-18 10:35:01 +0000

    feat: viewer api calls working

--- frontend/src/app/modules/viewer/components/landing-page/landing-page.component.ts
+++ frontend/src/app/modules/viewer/components/landing-page/landing-page.component.ts
@@ -1,16 +1,54 @@
-import { Component, inject } from '@angular/core';
+import { Component, DestroyRef, OnInit, inject } from '@angular/core';
 import { ViewerStateHandlerService } from '../../services/viewer-state-handler.service';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { filter, map } from 'rxjs';
+import { WorkdayAPIService } from '@app/shared/services/workday-api.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [MatButtonModule, RouterLink],
+  imports: [MatButtonModule, RouterLink, AsyncPipe],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss',
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit {
   // inject the services here
   private viewerStateHandlerService = inject(ViewerStateHandlerService);
+  private workdayAPIService = inject(WorkdayAPIService);
+  private destroyRef$ = inject(DestroyRef);
+
+  // router
+  private route = inject(ActivatedRoute);
+
+  ngOnInit(): void {
+    // fetch department query param
+    this.route.queryParams
+      .pipe(
+        takeUntilDestroyed(this.destroyRef$),
+        // set the department
+        map((params) => params['department']),
+        filter((department): department is string => !!department),
+        // fetch the current date
+        map((department) => {
+          const currentDate = new Date();
+
+          return {
+            department,
+            currentDate,
+          };
+        }),
+      )
+      .subscribe(({ department, currentDate }) => {
+        // set both the department and the current date
+        // This will cause the activeWeek signal to be updated
+        // and fetch all workdays for the current week we want to view
+        this.viewerStateHandlerService.setDepartment(department);
+        this.viewerStateHandlerService.setDate(currentDate);
+      });
+  }
+
+  activeWorkdays$ = this.viewerStateHandlerService.activeWorkdays$;
 }
--- frontend/src/app/modules/viewer/services/viewer-state-handler.service.ts
+++ frontend/src/app/modules/viewer/services/viewer-state-handler.service.ts
@@ -1,12 +1,33 @@
-import { Injectable, inject, signal } from '@angular/core';
+import { Injectable, computed, inject, signal } from '@angular/core';
+import { toObservable } from '@angular/core/rxjs-interop';
+import { Workday } from '@app/shared/interfaces/workday';
 import { WorkdayAPIService } from '@app/shared/services/workday-api.service';
+import { catchError, filter, from, map, mergeMap, of, reduce, tap } from 'rxjs';
+
+const getWeekFromDate = (date: Date) => {
+  // Create a new date object from the input date
+  const currentDate = new Date(date.getTime());
+
+  // Calculate the nearest Monday
+  while (currentDate.getDay() !== 1) {
+    currentDate.setDate(currentDate.getDate() - 1);
+  }
+
+  // Create an array to store all dates of the week
+  const week = [new Date(currentDate.getTime())];
+
+  // Add the next 6 days to the array
+  for (let i = 1; i <= 6; i++) {
+    currentDate.setDate(currentDate.getDate() + 1);
+    week.push(new Date(currentDate.getTime()));
+  }
+
+  return week;
+};
 
 type ActiveWeek = {
   department: string;
-  startDate: Date;
-  endDate: Date;
-  weekNumber: number;
-  year: number;
+  dates: Date[];
 };
 
 @Injectable({
@@ -17,5 +38,53 @@ export class ViewerStateHandlerService {
   private workdayAPIService = inject(WorkdayAPIService);
 
   // this keeps track of the active week currently being viewed
-  private activeWeek = signal<ActiveWeek | null>(null);
+  private activeDepartment$ = signal<string | null>(null);
+  private activeDate$ = signal<Date | null>(null);
+
+  // dynamically caluclated based on the activeDepartment and activeDate
+
+  activeWeek = computed<ActiveWeek | null>(() => {
+    if (!this.activeDepartment$() || !this.activeDate$()) {
+      return null;
+    }
+
+    return {
+      department: this.activeDepartment$()!,
+      dates: getWeekFromDate(this.activeDate$()!),
+    };
+  });
+
+  setDepartment(department: string) {
+    this.activeDepartment$.set(department);
+  }
+  setDate(date: Date) {
+    this.activeDate$.set(date);
+  }
+
+  // this keeps track of the active week currently being viewed
+  // this is a computed property which fetches the workdays upon receiving activeWeek signal
+  activeWorkdays$ = toObservable(this.activeWeek).pipe(
+    filter((activeWeek): activeWeek is ActiveWeek => !!activeWeek),
+    // date to YYYY-MM-DD format
+    map(({ department, dates }) => ({
+      department,
+      dates: dates.map((date) => date.toISOString().split('T')[0]),
+    })),
+    // call API for each date in dates and return the workdays for each date
+    mergeMap(({ department, dates }) =>
+      // convert the dates array to an observable
+      from(dates).pipe(
+        // fetch workdays for each date
+        mergeMap((date) =>
+          this.workdayAPIService.getWorkdays(department, date).pipe(
+            map((resp) => resp.data), // map the response to the data property
+            catchError(() => of([])), // if there is an error, return an empty array
+          ),
+        ),
+        reduce((acc, workdays) => [...acc, ...workdays], [] as Workday[]), // reduce the workdays into a single array
+      ),
+    ),
+    // debug code
+    tap((workdays) => console.log(workdays)),
+  );
 }
