e243e7cfc20f4524422e8f0b4abdee97e2e3ceb9
kajeepan kandeepan <kajeepan.kandeepan@gmail.com>
2024-01-30 09:04:05 +0000

    cleanup: adjusted project name in frontend

--- frontend/src/app/constants/constants.ts
+++ frontend/src/app/constants/constants.ts
@@ -14,8 +14,8 @@ export class constants {
     USERNAME_STORAGE: 'username',
   };
 
-  public static TITLE_SHORT = 'ZMT';
-  public static TITLE_LONG = 'ZMT - Zeitmanagement Tool';
+  public static TITLE_SHORT = 'TP';
+  public static TITLE_LONG = 'TP - Timeplanner';
   public static VERSION = '1.0.0';
 
   public static IS_PRODUCTION = environment.production;
