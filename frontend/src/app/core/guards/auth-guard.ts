1ee0d81f1129a0584b2d37e9fe70773ea55871e2
kajeepan kandeepan <kajeepan.kandeepan@gmail.com>
2024-01-31 10:11:51 +0000

    fix: auth guard user feedback

--- frontend/src/app/core/guards/auth-guard.ts
+++ frontend/src/app/core/guards/auth-guard.ts
@@ -1,19 +1,34 @@
 import { inject } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
-import { map } from 'rxjs';
+import { map, tap } from 'rxjs';
+import { messages } from '@app/constants/messages';
+import { NotificationService } from '../services/notification.service';
 
 // Guard to check if user has access to a department, TODO: fix me
 export const hasAccessToDepartmentGuard: CanActivateFn = (route: ActivatedRouteSnapshot) => {
   const authService = inject(AuthService);
   const router = inject(Router);
+  const notificationService = inject(NotificationService);
 
   // get department by query param
   const departmentFlag = route.queryParams['department'];
 
   // simply catch the error and return false if the user does not have access
-  return authService.hasAccessToDepartment(departmentFlag).pipe(map((hasAccess) => hasAccess || router.createUrlTree(['/'])));
+  return authService.hasAccessToDepartment(departmentFlag).pipe(
+    tap((hasAccess) => {
+      if (!hasAccess) notificationService.warnMessage(messages.AUTH.FORBIDDEN);
+    }),
+    map((hasAccess) => hasAccess || router.createUrlTree(['/'])),
+  );
 };
 
 // Simple guard to check if the user is authenticated
-export const isAuthenticated = () => inject(AuthService).isLoggedIn();
+export const isAuthenticated = () => {
+  const notificationService = inject(NotificationService);
+  const isLoggedIn = inject(AuthService).isLoggedIn();
+
+  if (!isLoggedIn) notificationService.warnMessage(messages.AUTH.UNAUTHORIZED);
+
+  return isLoggedIn;
+};
