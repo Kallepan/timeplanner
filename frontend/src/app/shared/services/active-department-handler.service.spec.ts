fc3c819bedc016ca44c5644cc0c4a913e6f3466a
kajeepan kandeepan <kajeepan.kandeepan@gmail.com>
2024-02-05 13:51:49 +0000

    feat: active department handler service test

--- frontend/src/app/shared/services/active-department-handler.service.spec.ts
+++ frontend/src/app/shared/services/active-department-handler.service.spec.ts
@@ -1,16 +1,43 @@
 import { TestBed } from '@angular/core/testing';
 
+import { ActivatedRoute } from '@angular/router';
 import { ActiveDepartmentHandlerService } from './active-department-handler.service';
 
 describe('ActiveDepartmentHandlerService', () => {
   let service: ActiveDepartmentHandlerService;
+  let mockActivatedRoute: jasmine.SpyObj<ActivatedRoute>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    mockActivatedRoute = jasmine.createSpyObj('ActivatedRoute', [], {
+      snapshot: {
+        queryParams: {
+          department: 'test',
+        },
+      },
+    });
+
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: mockActivatedRoute,
+        },
+      ],
+    });
     service = TestBed.inject(ActiveDepartmentHandlerService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get department from router query params', () => {
+    expect(service.activeDepartment$).toBe('test');
+  });
+
+  it('should handle undefined department', () => {
+    service.activeDepartment = undefined;
+
+    expect(service.activeDepartment$).toBe('');
   });
 });
--- frontend/src/app/shared/services/active-department-handler.service.ts
+++ frontend/src/app/shared/services/active-department-handler.service.ts
@@ -13,18 +13,21 @@ export class ActiveDepartmentHandlerService {
   // get department from router query params
   private activatedRoute = inject(ActivatedRoute);
 
-  private _activeDepartment = signal<string>(this._formatDepartment(this.activatedRoute.snapshot.queryParams['department']?.toLowerCase() ?? ''));
+  private _activeDepartment = signal<string>(this._formatDepartment(this.activatedRoute.snapshot.queryParams['department']));
 
   // or set department from code
-  set activeDepartment(department: string) {
+  set activeDepartment(department: string | undefined) {
     this._activeDepartment.set(this._formatDepartment(department));
   }
   get activeDepartment$() {
     return this._activeDepartment();
   }
 
-  private _formatDepartment(department: string) {
+  private _formatDepartment(department: string | undefined) {
     /* Ensure department is in lowercase and more if needed*/
+    if (!department) {
+      return '';
+    }
     return department.toLowerCase();
   }
 }
