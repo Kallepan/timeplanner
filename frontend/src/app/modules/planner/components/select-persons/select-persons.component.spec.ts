5aebf87564b9657e113bd748664adb8699d9d6d7
kajeepan kandeepan <kajeepan.kandeepan@gmail.com>
2024-02-07 10:39:26 +0000

    fix: select persons tests

--- frontend/src/app/modules/planner/components/select-persons/select-persons.component.spec.ts
+++ frontend/src/app/modules/planner/components/select-persons/select-persons.component.spec.ts
@@ -1,14 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SelectPersonsComponent } from './select-persons.component';
+import { PersonDataContainerService } from '@app/shared/services/person-data-container.service';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
 
 describe('SelectPersonsComponent', () => {
   let component: SelectPersonsComponent;
   let fixture: ComponentFixture<SelectPersonsComponent>;
+  let mockPersonDataContainerService: jasmine.SpyObj<PersonDataContainerService>;
 
   beforeEach(async () => {
+    mockPersonDataContainerService = jasmine.createSpyObj('PersonDataContainerService', ['getPersons']);
+
     await TestBed.configureTestingModule({
       imports: [SelectPersonsComponent],
+      providers: [{ provide: PersonDataContainerService, useValue: mockPersonDataContainerService }, provideNoopAnimations()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SelectPersonsComponent);
@@ -18,5 +24,14 @@ describe('SelectPersonsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit commentEditRequest', () => {
+    const button = fixture.nativeElement.querySelector('button#edit-comment-button');
+    spyOn(component.commentEditRequest, 'emit');
+
+    button.click();
+
+    expect(component.commentEditRequest.emit).toHaveBeenCalled();
   });
 });
