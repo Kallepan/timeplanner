23049ba0d81cdf28568d94e6840d9cdf7442f5d5
kajeepan kandeepan <kajeepan.kandeepan@gmail.com>
2024-02-26 12:15:02 +0000

    feat: absency panel in viewer

--- frontend/src/app/modules/viewer/components/viewer-landing-page/viewer-landing-page.component.spec.ts
+++ frontend/src/app/modules/viewer/components/viewer-landing-page/viewer-landing-page.component.spec.ts
@@ -5,6 +5,10 @@ import { ActiveDepartmentHandlerService } from '@app/shared/services/active-depa
 import { ActiveWeekHandlerService } from '@app/shared/services/active-week-handler.service';
 import { TimetableDataContainerService } from '@app/shared/services/timetable-data-container.service';
 import { ActivatedRoute } from '@angular/router';
+import { MatBottomSheet, MatBottomSheetRef } from '@angular/material/bottom-sheet';
+import { AbsencyPanelComponent } from '@app/modules/planner/components/absency-panel/absency-panel.component';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
+import { DepartmentAPIService } from '@app/shared/services/department-api.service';
 
 describe('ViewerLandingPageComponent', () => {
   let component: ViewerLandingPageComponent;
@@ -14,6 +18,9 @@ describe('ViewerLandingPageComponent', () => {
   let mockDepartmentHandlerService: jasmine.SpyObj<ActiveDepartmentHandlerService>;
   let mockActiveWeekHandlerService: jasmine.SpyObj<ActiveWeekHandlerService>;
   let mockTimeTableDataContainerService: jasmine.SpyObj<TimetableDataContainerService>;
+  let mockDepartmentAPIService: jasmine.SpyObj<DepartmentAPIService>;
+
+  let mockBottomSheet: jasmine.SpyObj<MatBottomSheet>;
 
   let mockActivatedRoute: jasmine.SpyObj<ActivatedRoute>;
 
@@ -23,6 +30,7 @@ describe('ViewerLandingPageComponent', () => {
     mockTimeTableDataContainerService = jasmine.createSpyObj('TimetableDataContainerService', [''], {
       isLoading$: false,
     });
+    mockDepartmentAPIService = jasmine.createSpyObj('DepartmentAPIService', ['']);
 
     mockActivatedRoute = jasmine.createSpyObj('ActivatedRoute', [''], {
       snapshot: {
@@ -33,12 +41,17 @@ describe('ViewerLandingPageComponent', () => {
       },
     });
 
+    mockBottomSheet = jasmine.createSpyObj('MatBottomSheet', ['open']);
+
     await TestBed.configureTestingModule({
       providers: [
         { provide: ActiveDepartmentHandlerService, useValue: mockDepartmentHandlerService },
         { provide: ActiveWeekHandlerService, useValue: mockActiveWeekHandlerService },
         { provide: TimetableDataContainerService, useValue: mockTimeTableDataContainerService },
+        { provide: DepartmentAPIService, useValue: mockDepartmentAPIService },
+        { provide: MatBottomSheet, useValue: mockBottomSheet },
         { provide: ActivatedRoute, useValue: mockActivatedRoute },
+        provideNoopAnimations(),
       ],
       imports: [ViewerLandingPageComponent],
     }).compileComponents();
@@ -69,5 +82,29 @@ describe('ViewerLandingPageComponent', () => {
     fixture.detectChanges();
 
     expect(overlay.style.display).toBe('block');
+  });
+
+  it('should toggle absency panel', () => {
+    component.toggleAbsencyPanel();
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    expect(mockBottomSheet.open).toHaveBeenCalledWith(AbsencyPanelComponent as any, { hasBackdrop: false, closeOnNavigation: true });
+  });
+
+  it('should dismiss absency panel', () => {
+    // return a dummy object
+    mockBottomSheet.open.and.returnValue({ dismiss: () => {} } as MatBottomSheetRef);
+
+    component.toggleAbsencyPanel();
+    fixture.detectChanges();
+    expect(component._bottomSheetRef).not.toBeNull();
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const bottomSheetRefSpy = spyOn(component._bottomSheetRef as any, 'dismiss');
+
+    component.toggleAbsencyPanel();
+
+    expect(bottomSheetRefSpy).toHaveBeenCalledTimes(1);
+    expect(mockBottomSheet.open).toHaveBeenCalledTimes(1);
   });
 });
--- frontend/src/app/modules/viewer/components/viewer-landing-page/viewer-landing-page.component.ts
+++ frontend/src/app/modules/viewer/components/viewer-landing-page/viewer-landing-page.component.ts
@@ -5,6 +5,8 @@ import { ActiveWeekHandlerService } from '@app/shared/services/active-week-handl
 import { TimetableDataContainerService } from '@app/shared/services/timetable-data-container.service';
 import { ViewOnlyTimetableComponent } from '../view-only-timetable/view-only-timetable.component';
 import { ActiveDepartmentHandlerService } from '@app/shared/services/active-department-handler.service';
+import { MatBottomSheet, MatBottomSheetConfig, MatBottomSheetRef } from '@angular/material/bottom-sheet';
+import { AbsencyPanelComponent } from '@app/modules/planner/components/absency-panel/absency-panel.component';
 
 @Component({
   selector: 'app-viewer-landing-page',
@@ -19,7 +21,24 @@ export class ViewerLandingPageComponent {
   activeWeekHandlerService = inject(ActiveWeekHandlerService);
   activeDepartmentHandlerService = inject(ActiveDepartmentHandlerService);
 
+  private _bottomSheet = inject(MatBottomSheet);
+  _bottomSheetRef: MatBottomSheetRef | null = null;
+
   getLoadingStatus() {
     return this.timetableDataContainerService.isLoading$;
+  }
+
+  toggleAbsencyPanel(): void {
+    if (this._bottomSheetRef) {
+      this._bottomSheetRef.dismiss();
+      this._bottomSheetRef = null;
+      return;
+    }
+
+    const config: MatBottomSheetConfig = {
+      hasBackdrop: false,
+      closeOnNavigation: true,
+    };
+    this._bottomSheetRef = this._bottomSheet.open(AbsencyPanelComponent, config);
   }
 }
