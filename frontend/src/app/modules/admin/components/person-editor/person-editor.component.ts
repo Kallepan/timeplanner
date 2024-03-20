67090ea23136cb798195c341d46c1eea18c1f8aa
kajeepan kandeepan <kajeepan.kandeepan@gmail.com>
2024-03-20 10:02:11 +0000

    feat: select person now displays that person

--- frontend/src/app/modules/admin/components/person-editor/person-editor.component.ts
+++ frontend/src/app/modules/admin/components/person-editor/person-editor.component.ts
@@ -18,4 +18,7 @@ export class PersonEditorComponent {
   setActivePerson(person: PersonWithMetadata) {
     this._personEditorDataContainerService.activePerson = person;
   }
+  getActivePerson() {
+    return this._personEditorDataContainerService.activePerson$;
+  }
 }
