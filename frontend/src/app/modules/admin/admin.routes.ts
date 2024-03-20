5b44ca768af38c8b73413b800650d62de90b6714
kajeepan kandeepan <kajeepan.kandeepan@gmail.com>
2024-03-20 06:30:50 +0000

    feat: routing to admin personnel editor

--- frontend/src/app/modules/admin/admin.routes.ts
+++ frontend/src/app/modules/admin/admin.routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from '@angular/router';
+import { ActiveDepartmentHandlerService } from '@app/shared/services/active-department-handler.service';
+import { DepartmentAPIService } from '@app/shared/services/department-api.service';
+import { PersonDataContainerService } from '@app/shared/services/person-data-container.service';
 
 export const routes: Routes = [
   {
@@ -15,6 +18,12 @@ export const routes: Routes = [
   },
   {
     path: 'person',
+    loadComponent: () => import('@app/modules/admin/components/person-editor-landing-page/person-editor-landing-page.component').then((m) => m.PersonEditorLandingPageComponent),
+    providers: [DepartmentAPIService],
+  },
+  {
+    path: 'person/detail',
     loadComponent: () => import('@app/modules/admin/components/person-editor/person-editor.component').then((m) => m.PersonEditorComponent),
+    providers: [PersonDataContainerService, ActiveDepartmentHandlerService],
   },
 ];
--- frontend/src/app/modules/admin/components/person-editor/person-editor.component.ts
+++ frontend/src/app/modules/admin/components/person-editor/person-editor.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { PersonAutocompleteComponent } from '@app/modules/absency/components/person-autocomplete/person-autocomplete.component';
 
 @Component({
   selector: 'app-person-editor',
   standalone: true,
-  imports: [],
+  imports: [PersonAutocompleteComponent],
   templateUrl: './person-editor.component.html',
-  styleUrl: './person-editor.component.scss'
+  styleUrl: './person-editor.component.scss',
 })
-export class PersonEditorComponent {
-
-}
+export class PersonEditorComponent {}
