549714cf22a053863002aea372fb657729881951
kajeepan kandeepan <kajeepan.kandeepan@gmail.com>
2024-03-20 06:40:04 +0000

    feat: sidenav implemented

--- frontend/src/app/core/components/sidenav/sidenav.component.ts
+++ frontend/src/app/core/components/sidenav/sidenav.component.ts
@@ -1,7 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink, RouterModule } from '@angular/router';
+import { AuthService } from '@app/core/services/auth.service';
+import { RouteHandlerService } from '@app/core/services/route-handler.service';
+import { ActiveDepartmentHandlerService } from '@app/shared/services/active-department-handler.service';
 @Component({
   selector: 'app-sidenav',
   standalone: true,
@@ -11,4 +14,17 @@ import { RouterLink, RouterModule } from '@angular/router';
 })
 export class SidenavComponent {
   @Output() closeSidenav = new EventEmitter<void>();
+
+  private routeHandlerService = inject(RouteHandlerService);
+  private activeDepartmentHandlerService = inject(ActiveDepartmentHandlerService);
+  private authService = inject(AuthService);
+
+  isAdmin() {
+    return this.authService.isAdmin$;
+  }
+  setActiveDepartment(department: string | undefined) {
+    this.activeDepartmentHandlerService.activeDepartment = department;
+  }
+
+  routeConfigurations = this.routeHandlerService.routeConfigurations();
 }
